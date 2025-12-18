1a732a9faeb1d077d19fab0e711c4d21b4f2e679
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2025-12-18 15:30:17 -0300

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Brayan Duwe on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
